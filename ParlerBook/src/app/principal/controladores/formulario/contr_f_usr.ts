1cb2e277a84b1cd8489617539b2d243f761b8d09
DarianMD <darianmanasturean@gmail.com>
2021-11-05 20:00:01 +0100

    valida los usuarios existentes

--- ParlerBook/src/app/principal/controladores/formulario/contr_f_usr.ts
+++ ParlerBook/src/app/principal/controladores/formulario/contr_f_usr.ts
@@ -1,10 +1,10 @@
 import { Reglas } from "../../models/reglas_campos";
 import Swal from "sweetalert2"
 import { personaje } from "../../usuario-registro/usuario-registro.component";
-import { Usuario } from "../../models/usuario";
 
 
 let reglas = new Reglas();
+let si = 0;
 
 
 
@@ -59,18 +59,23 @@ export class usr{
 
   usr_exist(usr:string): boolean{
 
-      if(usr == "paco"){
-        return true
-      }
-      else{
-        Swal.fire({
-          title: 'El usuario ya existe'
-        })
-        return false
+    for (let i = 0; i < personaje.length; i++) {
+      if (usr === String(personaje.map( i => i.usuario)[i])) {
+        si++;
       }
 
     }
-
-
+    if(si === 0){
+      return true;
+    }
+    else{
+        si = 0;
+        Swal.fire({
+          title: 'El usuario ya existe'
+        })
+      return false;
+    }
+}
 
 }
+
